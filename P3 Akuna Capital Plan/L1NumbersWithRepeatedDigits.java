d4c9cd240b0534e1e797b2347feb06c7eaffa23f
yangqianfan <qianfanyang@icloud.com>
2020-09-29 22:09:24 -0700

    Sep 29th Update

---
+++ P3 Akuna Capital Plan/L1NumbersWithRepeatedDigits.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class L1NumbersWithRepeatedDigits {
+    /**
+     * count res the number without repeated digit
+     * then the number with repeated digits = N - res
+     *
+     * Transform N + 1 arrayList
+     * count the number with digits < n
+     * count the number with same prefix
+     *
+     * for N = 8765,    L = [8, 7, 6, 6]
+     * XXX
+     * XX
+     * X
+     * 1XXX  -  7XXX
+     * 80XX  -  86XX
+     * 870X  -  875X
+     * 8760  -  8765
+     *
+     * @param N
+     * @return
+     */
+
+}

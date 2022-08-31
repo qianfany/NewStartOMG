c93d0cfc7d03f5892631811993d066f05eb20bfa
yangqianfan <qianfanyang@icloud.com>
2022-08-30 22:15:47 -0400

    L946 Validate Stack Sequences

--- 剑指Offer/JZ21IsStackPopOrder.java
+++ 剑指Offer/JZ21IsStackPopOrder.java
@@ -3,9 +3,32 @@ import java.util.Deque;
 
 public class JZ21IsStackPopOrder {
     /*
+    LeetCode 946:  Validate Stack Sequences
     1   2   3   4   5
     4   5   3   2   1
-    stack [ 1 2 3 4
+    stack
+    [ 1                         i = 0, index = 0
+    [ 1, 2                      i = 1, index = 0
+    [ 1, 2, 3                   i = 2, index = 0
+    [ 1, 2, 3, 4                i = 3, index = 0   stack.top = popped[0]     stack.poll, index++
+    [ 1, 2, 3                   stack.top != popped[1]
+    [ 1, 2, 3, 5                stack.top == popped[1] stack.poll, index++
+    [ 1, 2, 3                   stack.top == popped[2] stack.poll, index++
+    [ 1, 2                      stack.top == popped[3] stack.poll, index++
+    [ 1                         stack.top == popped[4] stack.poll, index++
+    [                           stack.isEmpty = true
+
+    1   2   3   4   5
+    4   3   5   1   2
+
+    [ 1                         i = 0, index = 0
+    [ 1, 2                      i = 1, index = 0
+    [ 1, 2, 3                   i = 2, index = 0
+    [ 1, 2, 3, 4                i = 3, index = 0   stack.top = popped[0]     stack.poll, index++
+    [ 1, 2, 3                   stack.top != popped[1]  i++
+    [ 1, 2, 3, 5                stack.top != popped[1]  at the end of the array
+    break the while loop
+    [ 1, 2, 3, 5                stack.isEmpty = false
 
     maintain a stack, add the element into the stack
     linear scan and add elements in pushA into the stack until match
@@ -27,6 +50,22 @@ public class JZ21IsStackPopOrder {
         for (int i = 0; i < pushA.length; i++) {
             stack.offerFirst(pushA[i]);
             while (!stack.isEmpty() && stack.peekFirst() == popA[index]) {
+                stack.pollFirst();
+                index++;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        if (pushed.length == 0 || popped.length == 0) {
+            return false;
+        }
+        int index = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < pushed.length; i++) {
+            stack.offerFirst(pushed[i]);
+            while (!stack.isEmpty() && stack.peekFirst() == popped[index]) {
                 stack.pollFirst();
                 index++;
             }

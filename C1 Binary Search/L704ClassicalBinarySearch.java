93b10ec49c6122b6071dc0353ae050a983d8d6db
Alan <wangyueleng@gmail.com>
2024-09-30 22:10:52 -0500

    New Start Day01

---
+++ C1 Binary Search/L704ClassicalBinarySearch.java
@@ -0,0 +1,25 @@
+public class L704ClassicalBinarySearch {
+    /**
+     * Binary Search
+     * sorted Array
+     * Time: O(log n)
+     * Space: O(1)
+     */
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
